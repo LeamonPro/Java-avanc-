9a4ab880e875e5a1c7d640be36d0ca236c4ed7da
Aymen <amriaymen221@gmail.com>
2022-11-15 22:11:43 +0100

    update

--- Tp/TP3/Window.java
+++ Tp/TP3/Window.java
@@ -11,6 +11,8 @@ public class Window extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Panel p1=new Panel();
         Panel p2=new Panel();
+        Panel p=new Panel();
+        p1.setLayout(new BorderLayout());
         JTable table=new JTable();
         String[] columns = {"Id","Produit","Prix"};
         Object[][] data = {
@@ -21,12 +23,40 @@ public class Window extends JFrame {
         DefaultTableModel model=new DefaultTableModel(data,columns);
         table.setModel(model);
         JScrollPane tpan=new JScrollPane(table);
-        p1.add(tpan);
+        p1.add(tpan,BorderLayout.NORTH);
+
+        p2.setLayout(new GridLayout(4,2));
+        JLabel l1=new JLabel("Id");
+        JTextField t1=new JTextField(20);
+        JLabel l2=new JLabel("Libelle");
+        JTextField t2=new JTextField(20);
+        JLabel l3=new JLabel("Prix");
+        JTextField t3=new JTextField(20);
         String[] list={"En Stock","En repture"};
+        JLabel l4=new JLabel("Disponibilité");
         JComboBox box=new JComboBox<>(list);
+        JButton b1=new JButton("ok");
+        JButton b2=new JButton("Annuler");
+        JButton b3=new JButton("Fermer");
+        p2.add(l1);
+        p2.add(t1);
+        p2.add(l2);
+        p2.add(t2);
+        p2.add(l3);
+        p2.add(t3);
+        p2.add(l4);
         p2.add(box);
-        p1.add(p2);
+        JPanel p3=new JPanel();
+        p.setLayout(new GridLayout(2,1));
+        p3.add(b1);
+        p3.add(b2);
+        p3.add(b3);
+        p.add(p2);
+        p.add(p3);
+
+        p1.add(p,BorderLayout.SOUTH);
         this.setContentPane(p1);
         this.setVisible(true);
     
 }}
+
